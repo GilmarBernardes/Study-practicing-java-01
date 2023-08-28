a9168accb03887fb4c12c571af4f373ba6e5aeb0
Gilmar Bernardes <galtran.void@gmail.com>
2023-08-28 20:38:08 +0000

    Praticing about primitive and reference variables

---
+++ _01/_01_hands_on/reference_variables/MenuItem.java
@@ -0,0 +1,16 @@
+package _01_hands_on.reference_variables;
+
+public class MenuItem {
+
+  private String name;
+
+  public MenuItem(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+
+}
