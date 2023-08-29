1ddb4445a8cb92d945651b5eae2c601921f3341b
Gilmar Bernardes <galtran.void@gmail.com>
2023-08-29 19:59:42 +0000

    praticing java related to basics of class, fields and methods

---
+++ _02/_02_hands_on/Ticket.java
@@ -0,0 +1,48 @@
+package _02_hands_on;
+
+// 1. Create a class called Ticket
+public class Ticket {
+
+  // 2. Give the class an empty constructor
+  public Ticket() {
+  }
+
+  /*
+   * 3. Create 3 private field variables in the class:
+   * - A field of type String called destination
+   * - A field called price of type double
+   * - A field called isReturn of type boolean
+   */
+  private String destination;
+  private double price;
+  private boolean isReturn;
+
+  // Add three methods to set the value of each field, called setDestination,
+  // setPrice and setIsReturn.
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
+  // Add a separate method to get the value of each field, called getDestination,
+  // getPrice and getIsReturn.
+
+  public String getDestination() {
+    return this.destination;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public boolean getIsReturn() {
+    return this.isReturn;
+  }
+}
