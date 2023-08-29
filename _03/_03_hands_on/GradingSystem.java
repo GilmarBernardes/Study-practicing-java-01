deed3418aca61cf0aff33864465f3da7e9473a29
Gilmar Bernardes <galtran.void@gmail.com>
2023-08-29 20:42:58 +0000

    Praticing basics of if-else statements

---
+++ _03/_03_hands_on/GradingSystem.java
@@ -0,0 +1,55 @@
+package _03_hands_on;
+
+public class GradingSystem {
+
+  public boolean isAPass(int percentage) {
+    // Return true if the percentage is higher than or equal to 60.
+    // Otherwise return false.
+    if (percentage >= 60) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public char getGrade(int percentage) {
+    // If the percentage is 90 or above, return 'A'.
+    // If it's 80-89, return 'B'.
+    // If it's 70-79, return 'C'.
+    // If it's 60-69, return 'D'.
+    // If it's less than 60, return 'F'.
+    // we can use a switch case here but let's just do what the exercise is asking
+    // us to do
+    if (percentage > 90) {
+      return 'A';
+    } else if (percentage > 80 && percentage <= 89) {
+      return 'B';
+    } else if (percentage > 70 && percentage <= 79) {
+      return 'C';
+    } else if (percentage > 60 && percentage <= 69) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  }
+
+  public String retakeMessage(int percentage, boolean allowedToRetake) {
+    String message = "";
+    // If percentage is less than 60 and allowedToRetake is true, return a String
+    // that says "The student has been entered for a retake."
+    // If percentage is less than 60 and allowedToRetake is false, return a String
+    // that says "The student is not allowed to retake this exam."
+    // If percentage is 60 or higher, return a String that says "A retake is not
+    // required."
+    if (percentage >= 60) {
+      message = "A retake is not required";
+    } else {
+      if (allowedToRetake) {
+        message = "The student has been entered for a retake";
+      } else {
+        message = "The student is now allowed to retake this exam";
+      }
+    }
+    return message;
+  }
+}
